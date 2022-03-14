27435b4d0bfaa2b0395c1f7584e9a30297e49813
Giovanni Orozco <whoisyio@gmail.com>
2022-03-13 22:55:26 -0600

    Conexion al socket

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var app=express();
 var server=require('http').Server(app);
 var io= require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/holamundo',function(req,res){
 res.status(200).send('hola mundo desde ruta')
+})
+
+io.on('connection',function(socket){
+    console.log("El nodo con ID: "+socket.handshake.address+" se ha conectado");
 })
 
 server.listen(6677,function(){
