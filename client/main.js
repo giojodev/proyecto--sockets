8301c96fc38cad3e26ea9d3f8a0372bcc5aaa00b
Giovanni Orozco <whoisyio@gmail.com>
2022-03-14 22:36:44 -0600

    Envio y recepcion de mensajes

--- client/main.js
+++ client/main.js
@@ -15,5 +15,18 @@ function render(data){
         </div>
         `);
 }).join(' ');
-document.getElementById('messages').innerHTML= html;
+var div_msgs=document.getElementById('messages');
+div_msgs.innerHTML=html;
+div_msgs.scrollTop=div_msgs.scrollHeight;
+}
+function addMessage(e){
+    var message= {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+
+    document.getElementById('nickname').style.display='none';
+    socket.emit('add-message',message)
+
+    return false
 }
